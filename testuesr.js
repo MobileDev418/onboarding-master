27bd1c6c0a5dbb6e38ba4800d658796c7342e94a
MobileDev418 <oncemoreforyou@outlook.com>
2019-01-30 09:52:53 +0800

    added test user

---
+++ testuesr.js
@@ -0,0 +1,101 @@
+const nurse = {"_id":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","firstname":"Phillip","lastname":"Tanek","email":"test@gmail.com","password":"123456","photo":"https://randomuser.me/api/portraits/men/4.jpg"}	
+const nursemodel = [
+	nurse
+];
+const residentmodel1 =    {"_id":"68E60617-7943-CE5B-46BE-2A43DA9B57D6","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2017-06-19T17:42:03-07:00","firstname":"Kevin","lastname":"Leroy","photo":"https://randomuser.me/api/portraits/men/47.jpg","room":5,"floor":1,"curroom":4,"curfloor":3,"location":"37.78825, -122.4324","iswandering":0};
+const residentmodel2 = {"_id":"BB288C5E-A996-EF7E-062D-1949B40D339C","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2017-08-29T05:29:48-07:00","firstname":"Callum","lastname":"Alan","photo":"https://randomuser.me/api/portraits/men/13.jpg","room":8,"floor":1,"curroom":3,"curfloor":2,"location":"37.78835, -122.4334","iswandering":1}
+const residentmodel3 = {"_id":"64715E2E-1A78-86AA-B5F4-115AF7B45362","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2018-03-31T07:41:25-07:00","firstname":"Palmer","lastname":"Stone","photo":"https://randomuser.me/api/portraits/women/22.jpg","room":9,"floor":2,"curroom":9,"curfloor":1,"location":"37.78845, -122.4334","iswandering":0}
+const residentmodel4 = {"_id":"55B36391-0E9A-EA84-8265-3E3D41164B57","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2019-03-17T21:01:31-07:00","firstname":"Lamar","lastname":"Jarrod","photo":"https://randomuser.me/api/portraits/women/53.jpg","room":5,"floor":1,"curroom":2,"curfloor":3,"location":"37.78855, -122.4354","iswandering":0}
+const residentmodel5 = {"_id":"DB095358-1245-687D-69D5-2B06C020686D","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2018-07-09T15:36:37-07:00","firstname":"Zane","lastname":"Zahir","photo":"https://randomuser.me/api/portraits/men/19.jpg","room":8,"floor":2,"curroom":8,"curfloor":2,"location":"37.78865, -122.4364","iswandering":0}
+const residentmodel6 = {"_id":"3B549AD9-7DA2-59E0-E983-6A703612F538","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2019-02-01T03:21:49-08:00","firstname":"Connor","lastname":"Dillon","photo":"https://randomuser.me/api/portraits/women/30.jpg","room":6,"floor":2,"curroom":8,"curfloor":3,"location":"37.78875, -122.4374","iswandering":1}
+const residentmodel7 = {"_id":"5D207B84-E292-7082-48F6-FDBB0B8EAF06","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2017-12-09T21:50:45-08:00","firstname":"Nathaniel","lastname":"Wesley","photo":"https://randomuser.me/api/portraits/men/95.jpg","room":5,"floor":1,"curroom":3,"curfloor":1,"location":"37.78885, -122.4384","iswandering":0}
+const residentmodel8 = {"_id":"DC9073C3-C176-D13A-9BD4-8AA03B486F8E","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2018-01-10T20:20:03-08:00","firstname":"Ahmed","lastname":"Hyatt","photo":"https://randomuser.me/api/portraits/women/36.jpg","room":6,"floor":3,"curroom":9,"curfloor":3,"location":"37.78895, -122.4394","iswandering":0}
+const residentmodel9 = {"_id":"AD21A7BB-11A3-AED6-CFF9-1E6AA8914C54","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2017-12-15T03:51:51-08:00","firstname":"Kenneth","lastname":"Tyrone","photo":"https://randomuser.me/api/portraits/men/37.jpg","room":7,"floor":2,"curroom":9,"curfloor":3,"location":"37.78925, -122.4404","iswandering":0}
+const residentmodel10 = {"_id":"AD7CF2D4-FE21-6FBA-3F0C-3177A4ABE20F","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2017-11-19T23:07:51-08:00","firstname":"Lucius","lastname":"Dillon","photo":"https://randomuser.me/api/portraits/men/38.jpg","room":7,"floor":3,"curroom":7,"curfloor":3,"location":"37.78935, -122.4414","iswandering":0}
+const residentmodel11 = {"_id":"6FC74A17-CCEA-E05B-92EB-C90A7303162C","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2019-04-22T10:11:39-07:00","firstname":"Colby","lastname":"Declan","photo":"https://randomuser.me/api/portraits/women/48.jpg","room":2,"floor":1,"curroom":2,"curfloor":2,"location":"37.78945, -122.4424","iswandering":1}
+const residentmodel12 = {"_id":"16D9C189-3284-6ADC-D0DE-604920BEDB92","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2018-07-10T02:29:39-07:00","firstname":"John","lastname":"Barrett","photo":"https://randomuser.me/api/portraits/men/14.jpg","room":6,"floor":3,"curroom":6,"curfloor":1,"location":"37.78955, -122.4434","iswandering":1}
+const residentmodel13 = {"_id":"B8C747BB-C69F-A2FE-E49D-36676DD7B51D","receiverid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","lastupdate":"2018-03-23T17:04:53-07:00","firstname":"Salvador","lastname":"Igor","photo":"https://randomuser.me/api/portraits/men/11.jpg","room":6,"floor":2,"curroom":1,"curfloor":2,"location":"37.78965, -122.4444","iswandering":1}
+
+const notemodel1 =	{"_id":"224FBCE5-051F-FDBC-C67C-ED2BC1976220","createdAt":"2018-02-09T05:30:15-08:00","type":0,"category":3,"content":"id, mollis nec, cursus a, enim. Suspendisse aliquet, sem ut cursus luctus, ipsum leo elementum sem, vitae aliquam eros turpis","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel2 ={"_id":"595EEF85-7D11-A271-A894-A2280D90043F","createdAt":"2017-11-09T14:17:06-08:00","type":0,"category":5,"content":"dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec fringilla. Donec feugiat","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel3 ={"_id":"26073E36-CF14-404F-2F1D-06F2906FA921","createdAt":"2017-09-12T02:41:50-07:00","type":0,"category":7,"content":"faucibus lectus, a sollicitudin orci sem eget massa. Suspendisse eleifend. Cras sed leo. Cras vehicula aliquet libero. Integer in magna.","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel4 ={"_id":"EBC272BE-453B-4DA5-B830-7F9DB1D018ED","createdAt":"2018-03-27T02:32:22-07:00","type":0,"category":10,"content":"Maecenas ornare egestas ligula. Nullam feugiat placerat velit. Quisque varius. Nam porttitor scelerisque neque. Nullam nisl. Maecenas malesuada fringilla est.","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel5 ={"_id":"08ACBE78-9ACC-E172-EC4F-133028D3DCDB","createdAt":"2017-09-03T06:00:04-07:00","type":0,"category":0,"content":"interdum feugiat. Sed nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor,","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel6 ={"_id":"E12801CA-B80E-71E0-9D3C-04C319D4FA3A","createdAt":"2019-05-28T14:36:51-07:00","type":0,"category":2,"content":"hymenaeos. Mauris ut quam vel sapien imperdiet ornare. In faucibus. Morbi vehicula. Pellentesque tincidunt tempus risus. Donec egestas. Duis ac","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel7 ={"_id":"01432A20-6E0A-9E1F-EA5F-87984F7FD9E8","createdAt":"2017-10-22T21:17:25-07:00","type":0,"category":4,"content":"eleifend non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero. Proin sed turpis nec mauris blandit mattis. Cras","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel8 ={"_id":"6F514280-3ED1-CDCC-440E-9EE9425B667C","createdAt":"2018-05-18T17:37:01-07:00","type":1,"category":3,"content":"nunc. In at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum ac mi eleifend egestas. Sed pharetra, felis eget","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel9 ={"_id":"5A74AED8-231B-8CDF-88D1-C96D0B70DC20","createdAt":"2018-02-19T04:23:10-08:00","type":0,"category":6,"content":"Mauris eu turpis. Nulla aliquet. Proin velit. Sed malesuada augue ut lacus. Nulla tincidunt, neque vitae semper egestas, urna justo","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+const notemodel10 ={"_id":"AF5A5E3E-72CA-7EA0-9754-3D71CE57CC5B","createdAt":"2018-12-27T06:44:00-08:00","type":0,"category":9,"content":"velit. Sed malesuada augue ut lacus. Nulla tincidunt, neque vitae semper egestas, urna justo faucibus lectus, a sollicitudin orci sem","senderid":"09CEE95A-ED9B-CE04-6618-207F1E8404E0","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+
+
+const residentmodel = [
+        residentmodel1,
+        residentmodel2,
+        residentmodel3,
+        residentmodel4,
+        residentmodel5,
+        residentmodel6,
+        residentmodel7,
+        residentmodel8,
+        residentmodel9,
+        residentmodel10,
+        residentmodel11,
+        residentmodel12,
+        residentmodel13
+];
+const notifications = [
+	{"_id":"1112C138-EE75-41F7-5647-53E0E0B5E050","createdAt":"2019-05-21T11:50:25-07:00","type":1,"status":2,"sender":nurse,"receiver":residentmodel1, "note" : "" },
+	{"_id":"44FAE605-6772-8E53-2195-AED7BCFAB600","createdAt":"2018-07-22T00:02:11-07:00","type":0,"status":2,"sender":nurse,"receiver":residentmodel2, "note" : ""}, 
+	{"_id":"42081C9E-0F74-9256-60DA-6C92CFD452AB","createdAt":"2018-02-13T13:34:11-08:00","type":0,"status":0,"sender":nurse,"receiver":residentmodel3, "note" : ""},
+	{"_id":"9A3CB312-C3E4-510C-9FDC-70B133EBD92C","createdAt":"2017-12-03T02:45:29-08:00","type":1,"status":2,"sender":nurse,"receiver":residentmodel4, "note" : notemodel6},
+	{"_id":"0787815C-C2A5-621E-F15B-6B45A311E0D1","createdAt":"2018-06-30T22:48:23-07:00","type":1,"status":0,"sender":nurse,"receiver":residentmodel5, "note" : ""},
+	{"_id":"49EAEF10-A753-5266-433B-A4B1CC3A7AA3","createdAt":"2019-06-02T20:49:57-07:00","type":0,"status":2,"sender":nurse,"receiver":residentmodel6, "note" : notemodel4},
+	{"_id":"A63AD35A-C9DC-4E64-73B7-70701725AAD4","createdAt":"2018-06-07T07:25:19-07:00","type":1,"status":2,"sender":nurse,"receiver":residentmodel7, "note" : notemodel3},
+	{"_id":"41F5CD75-4C1E-0521-4073-85E7376D147B","createdAt":"2017-11-17T04:59:31-08:00","type":1,"status":1,"sender":nurse,"receiver":residentmodel8, "note" : ""},
+	{"_id":"879718AB-452F-63E5-698E-E36EE56E53D6","createdAt":"2019-06-02T10:21:18-07:00","type":1,"status":2,"sender":nurse,"receiver":residentmodel9, "note" : notemodel10},
+	{"_id":"7A965E90-BE1E-6BC8-55A7-D2D55DE698F1","createdAt":"2018-10-25T03:30:36-07:00","type":0,"status":2,"sender":nurse,"receiver":residentmodel10, "note" : ""}	
+]
+
+
+
+const notemodel = [
+    notemodel1,
+    notemodel2,
+    notemodel3,
+    notemodel4,
+    notemodel5,
+    notemodel6,
+    notemodel7,
+    notemodel8,
+    notemodel9,
+    notemodel10,
+]
+
+
+
+const batterymodel = 
+[
+    {"_id":"78AA7994-E538-8BAA-0551-18308167DEA1","amount":23,"lastupdate":"2018-08-26T02:10:51-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"488FDF6B-2424-5710-A83B-5CD77E69F2ED","amount":46,"lastupdate":"2019-05-29T06:46:21-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"8860DE26-83DA-EF2D-DA7E-A85829AF7698","amount":70,"lastupdate":"2019-05-27T08:19:47-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"11FFEB7A-606D-3C8E-7F40-0BA2C5573308","amount":36,"lastupdate":"2018-12-02T06:16:18-08:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"4DC1FC58-375E-F2BF-2ED7-B555AEAA7EF0","amount":57,"lastupdate":"2017-07-18T05:09:01-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"406162D0-EAFC-1D7D-D37F-CB5C2253E19F","amount":42,"lastupdate":"2019-02-23T09:11:49-08:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"0D4EEC8D-0E39-3D3D-819F-FB6146C16F86","amount":46,"lastupdate":"2019-05-29T13:47:06-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"9821C883-5413-2CCA-616A-D5623D3CCE24","amount":62,"lastupdate":"2017-07-04T08:51:59-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"FFDAE21B-A568-8CBD-4163-7037CB3C18FA","amount":52,"lastupdate":"2017-08-24T07:58:32-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"BAD90D6C-E2F0-3EFD-ECD8-199E14434B65","amount":33,"lastupdate":"2019-01-12T16:27:04-08:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"CFC06B35-31C2-D730-C08A-20A3AD02744D","amount":58,"lastupdate":"2019-01-17T04:15:46-08:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"4D0567D0-2A42-E634-2C0C-6FD765C999EF","amount":49,"lastupdate":"2019-03-06T12:45:15-08:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"B2F81A45-310F-1C57-9F15-CBEC3A03E919","amount":57,"lastupdate":"2017-08-03T07:48:14-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"337593FE-4B32-1677-6DAA-32390C89297E","amount":65,"lastupdate":"2017-10-29T05:51:49-07:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"},
+    {"_id":"C3BA7FD2-49D4-5D23-F87A-DF70178956AF","amount":35,"lastupdate":"2018-01-12T08:45:22-08:00","receiverid":"68E60617-7943-CE5B-46BE-2A43DA9B57D6"}
+]
+
+export {
+    batterymodel,
+    notifications,
+    notemodel,
+    residentmodel,
+    nursemodel
+}
